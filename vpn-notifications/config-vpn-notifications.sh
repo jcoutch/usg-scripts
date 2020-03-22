f4f6af31ba5b0460ec6cf3a811b712a6b4997fdb
Geos <1248414+G-eos@users.noreply.github.com>
2020-03-22 18:23:14 +0100

    Prepare for cron configured by gateway.json
    Move SSMTP config into cron ran file through cheking that SSMTP files are available.

--- vpn-notifications/config-vpn-notifications.sh
+++ vpn-notifications/config-vpn-notifications.sh
@@ -12,48 +12,6 @@ source /config/scripts/post-config.d/parameter.env
 source /opt/vyatta/etc/functions/script-template
 readonly logFile="/var/log/config-smtp.log"
 
-# Write aliases config
-cat > /etc/ssmtp/revaliases <<EOF
-# sSMTP aliases
-# 
-# Format:	local_account:outgoing_address:mailhub
-#
-# Example: root:your_login@your.domain:mailhub.your.domain[:port]
-# where [:port] is an optional port number that defaults to 25.
-root:$EmailAddress:$MailServer:$MailPort
-$RouterUser:$EmailAddress:$MailServer:$MailPort
-www-data:$EmailAddress:$MailServer:$MailPort
-EOF
-
-# Write SMTP config
-cat > /etc/ssmtp/ssmtp.conf <<EOF
-#
-# Config file for sSMTP sendmail
-#
-# The person who gets all mail for userids < 1000
-# Make this empty to disable rewriting.
-root=$EmailAddress
-
-# The place where the mail goes. The actual machine name is required no 
-# MX records are consulted. Commonly mailhosts are named mail.domain.com
-mailhub=$MailServer:$MailPort
-
-AuthUser=$AuthUser
-AuthPass=$Password
-UseSTARTTLS=YES
-
-# Where will the mail seem to come from?
-rewriteDomain=
-
-# The full hostname
-hostname=$HostName
-
-# Are users allowed to set their own From: address?
-# YES - Allow the user to specify their own From: address
-# NO - Use the system generated From: address
-FromLineOverride=YES
-EOF
-
 # Add a scheduled task to send the e-mails every minute
 configure
 set system task-scheduler task check-vpn-connections executable path "/config/scripts/post-config.d/notify-on-vpn-state-change.sh"
--- vpn-notifications/notify-on-vpn-state-change.sh
+++ vpn-notifications/notify-on-vpn-state-change.sh
@@ -12,6 +12,59 @@ source /config/scripts/post-config.d/parameter.env
 source /opt/vyatta/etc/functions/script-template
 run=/opt/vyatta/bin/vyatta-op-cmd-wrapper
 
+
+# Verify and prepare SSMTP configuration
+if [ ! -f "/etc/ssmtp/revaliases" ]; 
+then
+# Write aliases config
+cat > /etc/ssmtp/revaliases <<EOF
+# sSMTP aliases
+# 
+# Format:       local_account:outgoing_address:mailhub
+#
+# Example: root:your_login@your.domain:mailhub.your.domain[:port]
+# where [:port] is an optional port number that defaults to 25.
+root:$EmailAddress:$MailServer:$MailPort
+$RouterUser:$EmailAddress:$MailServer:$MailPort
+www-data:$EmailAddress:$MailServer:$MailPort
+EOF
+
+fi
+
+if [ ! -f "/etc/ssmtp/ssmtp.conf" ];
+then
+
+# Write SMTP config
+cat > /etc/ssmtp/ssmtp.conf <<EOF
+#
+# Config file for sSMTP sendmail
+#
+# The person who gets all mail for userids < 1000
+# Make this empty to disable rewriting.
+root=$EmailAddress
+
+# The place where the mail goes. The actual machine name is required no 
+# MX records are consulted. Commonly mailhosts are named mail.domain.com
+mailhub=$MailServer:$MailPort
+
+AuthUser=$AuthUser
+AuthPass=$Password
+UseSTARTTLS=YES
+
+# Where will the mail seem to come from?
+rewriteDomain=
+
+# The full hostname
+hostname=$HostName
+
+# Are users allowed to set their own From: address?
+# YES - Allow the user to specify their own From: address
+# NO - Use the system generated From: address
+FromLineOverride=YES
+EOF
+
+fi	
+
 # Init the temp files
 touch /tmp/temp.vpnconnections
 touch /tmp/temp.vpnconnections2
